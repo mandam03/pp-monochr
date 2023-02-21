fdf9842a45186e0ac194c2f7cc05df2f76f424b4
mandam03 <mmiha_49@yahoo.com>
2023-02-21 02:01:36 +0200

    pp-monochr-

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,20 @@
 import Image from 'next/image';
 
 export default function Home() {
-  return <div>proiect personal Monochrome</div>;
-
-  <Image></Image>;
+  return (
+    <div>
+      Monochrome
+      <img src="/images/banners/banner.png" alt="banner" />
+      <button className="buton button-primary button-large">
+        <a href="/">View all</a>
+      </button>
+      <button class="button button-inverted">
+        <a href="/">View all</a>
+      </button>
+      <button class="button button-small">
+        <a href="/">Submit</a>
+      </button>
+      <a class="button button-primary">New in</a>
+    </div>
+  );
 }
